99aaa940262194cc4b35acd70a2cde4d686a5059
Samuel Waugh <swaugh@rodeofx.com>
2018-07-09 16:26:14 -0400

    formating tests

--- model/framework/testsuite.lua
+++ model/framework/testsuite.lua
@@ -5,64 +5,72 @@ local state = require('state')
 local json = require('json_reader')
 local pprint = function (value) local inspect = require("inspect") print(inspect(value)) end
 
-state.evolve('Version', '0.0.0.0.5')
-state.evolve('mc', {['name']='Chaos'})
-state.evolve('slglobal', {['Aeon']={level=1, angle=0}})
-state.savestate(nil)
+function setState()
+	state.evolve('Version', '0.0.0.0.5')
+	state.evolve('mc', {['name']='Chaos'})
+	state.evolve('slglobal', {['Aeon']={level=1, angle=0}})
+	state.savestate(nil)
+end
 
-json.read({file="shopmenus.json"})
+function shop()
+	json.read({file="shopmenus.json"})
+	state.changecontext('shop', 'trainer')
+	state.event(json.encode({key="shop.nav.menu", index=0}))
+end
 
-state.changecontext('shop', 'trainer')
+function link()
+	state.changecontext('link', {arcana='Aeon'})
 
-state.event(json.encode({key="shop.nav.menu", index=0}))
+	state.event(json.encode({key="link.action", index=0}))
+	state.event(json.encode({key="link.action", index=0}))
+	state.event(json.encode({key="link.action", index=0}))
+	state.event(json.encode({key="link.action", index=0}))
+	state.event(json.encode({key="link.action", index=0}))
+	state.event(json.encode({key="link.action", index=1}))
+end
 
+function battle()
+	--LEGACY/PROOF OF CONCEPT
+	--Based on receiving individual inputs
+	shadowep=json.read({file='Seraph.json'})
+	aigis=json.read({file='Cherub.json'})
+	mc=json.read({file='Dominion.json'})
 
-state.changecontext('link', {arcana='Aeon'})
+	state.changecontext('battle', {['party']={{['name']='Aigis', ['persona']=aigis}, {['name']='MC', ['persona']=mc}}, ['ene']={{['name']='Shadow', ['persona']=shadowep}}})
 
-state.event(json.encode({key="link.action", index=0}))
+	state.input('select')
+	state.input('select')
+	state.input('select')
 
-state.event(json.encode({key="link.action", index=0}))
+	state.savestate(nil)
 
-state.event(json.encode({key="link.action", index=0}))
+	state.changecontext('link', {arcana='Aeon', level='1', angle='0'})
 
-state.event(json.encode({key="link.action", index=0}))
+	state.input('select')
+	state.input('select')
+	state.input('select')
+	state.input('select')
+	state.input('select')
+	state.input('up')
+	state.input('select')
+	state.input('select')
+end
 
-state.event(json.encode({key="link.action", index=0}))
+function test(feature)
+	setState()
+	feature()
+	print("\nState")
+	--pprint(state)
+	for key, value in pairs(state) do print(key, value) end
+	print("\nContext")
+	--pprint(state.context)
+	for key, value in pairs(state.context) do print(key, value) end
+	print("\nRefresh")
+	--pprint(json.decode(state.update))
+	for key, value in pairs(json.decode(state.update)) do print(key, value) end
+	--print(#state.update.options.." options")
+end
 
-state.event(json.encode({key="link.action", index=1}))
-
-
---LEGACY/PROOF OF CONCEPT
-shadowep=json.read({file='Seraph.json'})
-aigis=json.read({file='Cherub.json'})
-mc=json.read({file='Dominion.json'})
-
-state.changecontext('battle', {['party']={{['name']='Aigis', ['persona']=aigis}, {['name']='MC', ['persona']=mc}}, ['ene']={{['name']='Shadow', ['persona']=shadowep}}})
-
-state.input('select')
-state.input('select')
-state.input('select')
-
-state.savestate(nil)
-
-state.changecontext('link', {arcana='Aeon', level='1', angle='0'})
-
-state.input('select')
-state.input('select')
-state.input('select')
-state.input('select')
-state.input('select')
-state.input('up')
-state.input('select')
-state.input('select')
-
-print("\nState")
---pprint(state)
-for key, value in pairs(state) do print(key, value) end
-print("\nContext")
---pprint(state.context)
-for key, value in pairs(state.context) do print(key, value) end
-print("\nRefresh")
---pprint(json.decode(state.update))
-for key, value in pairs(json.decode(state.update)) do print(key, value) end
---print(#state.update.options.." options")
+--test(battle)
+--test(link)
+test(shop)
