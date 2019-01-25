533792e308890b5b9132dae5ed6f9aa9a42c0217
Samuel Waugh <swaugh@rodeofx.com>
2019-01-25 16:35:55 -0500

    updated spell files

--- model/data/spells/Chaos!.lua
+++ model/data/spells/Chaos!.lua
@@ -20,7 +20,7 @@ spell["costtype"] = [[HP/SP(%)]]
 
 function spell.activate()
     state.context.cost(spell.costtype, spell.cost)
-    state.context.attack(spell.numericalvalue, spell.targetattribute, spell.numericaltype, spell.numberofhits)
+    state.context.attack(spell, state.battle.participants[state.battle.target], state.battle.participants[state.battle.open])
 end
 
 return spell
--- model/data/spells/Recarm.lua
+++ model/data/spells/Recarm.lua
@@ -18,7 +18,7 @@ spell["costtype"] = [[SP]]
 
 function spell.activate()
     state.context.cost(spell.costtype, spell.cost)
-    state.context.attack(spell.numericalvalue, spell.targetattribute, spell.numericaltype, spell.numberofhits)
+    state.context.attack(spell, state.battle.participants[state.battle.target], state.battle.participants[state.battle.open])
 end
 
 return spell
