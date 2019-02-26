a3c67e1e3f8d9204727cf80efff4ef9e532b7c31
Samuel Waugh <swaugh@rodeofx.com>
2019-02-26 10:31:11 -0500

    edited C++ testsuite for new state variable init

--- controller/setupState.cpp
+++ controller/setupState.cpp
@@ -12,7 +12,7 @@ extern "C"{
 /*Defines runtime Lua import path, and imports the state, which instantiates the game state
 and sets it as global variable. Defined for explicit runtime. Executable MUST be one level
 above model with this current const value.*/
-const char * const LUA_LOCAL_PREP = "package.path = package.path..';./extlib/lua_path/?.lua;./model/?.lua;' package.cpath = package.cpath..';./extlib/lua_cpath/?.dll' require('state')";
+const char * const LUA_LOCAL_PREP = "package.path = package.path..';./extlib/lua_path/?.lua;./model/?.lua;' package.cpath = package.cpath..';./extlib/lua_cpath/?.dll' require('state') require('util/state/start_game')";
 
 static lua_State* prepLuaState(){
     /**
