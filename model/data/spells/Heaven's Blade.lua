b81c14719e70df8444071798448cc6b09910ed3f
Samuel Waugh <swaugh@rodeofx.com>
2019-03-11 12:13:56 -0400

    updated standard damage spells for new battleattack

--- model/data/spells/Heaven's Blade.lua
+++ model/data/spells/Heaven's Blade.lua
@@ -17,7 +17,7 @@ spell["costtype"] = [[HP (%)]]
 
 function spell.activate()
     state.context.cost(spell.costtype, spell.cost)
-    state.context.attack(spell, state.battle.participants[state.battle.target], state.battle.participants[state.battle.open])
+    state.context.attack(spell, state.battle.participants[state.battle.open])
 end
 
 return spell
