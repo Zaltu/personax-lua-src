d94496310afdfe85432637206c6a3551ccb9e288
Samuel Waugh <swaugh@rodeofx.com>
2019-03-12 11:46:44 -0400

    ghastly wail

---
+++ model/data/spells/Ghastly Wail.lua
@@ -0,0 +1,24 @@
+local spell = {}
+spell["element"] = [[Almighty]]
+spell["cost"] = 15
+spell["desc"] = [[Instantly kill anyone inflicted with fear]]
+spell["target"] = [[All]]
+spell["name"] = [[Ghastly Wail]]
+spell["costtype"] = [[SP]]
+
+function spell.activate()
+    turntable = {}
+    for name, participant in pairs(state.battle.participants) do
+        if participant.status == "Fear" then
+            damage = participant.hp
+            participant.hp = 0
+            table.insert(turntable, {
+                target=name,
+                caster=state.battle.participants[state.battle.open],
+                damage=damage,
+                dmgType="HP"
+            })
+    if #turntable > 0 then table.insert(state.battle.turns, turntable) end
+end
+
+return spell
