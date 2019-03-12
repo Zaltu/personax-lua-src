3eeb41e9a38c7debf56bc876c85e0cef0fc3128a
Samuel Waugh <swaugh@rodeofx.com>
2019-03-12 15:33:10 -0400

    ailment boost

---
+++ model/data/spells/Ailment Boost.lua
@@ -0,0 +1,20 @@
+local spell = {}
+spell["element"] = [[Status]]
+spell["desc"] = [[Raises chance to inflict statuses on the enemy]]
+spell["passive"] = true
+spell["statuschance"] = 0.25
+spell["name"] = [[Ailment Boost]]
+
+function spell.activate()
+    state.context.passive(spell, state.battle.participants[state.battle.open], state.battle.participants[state.battle.open], 99, "dodgestatus")
+end
+
+function spell.process(uspell, basehitchance, istarget)
+    if not uspell.status then return 0 end
+    if istarget then
+        return 0
+    end
+    return basehitchance * spell.numericalvalue
+end
+
+return spell
